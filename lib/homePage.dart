d018986819900cb5dc9448f5bd599b2ea6b442ea
Misbah Haque <misbahmuneer2003@gmail.com>
2025-03-21 11:00:35 +0500

    Update

---
+++ lib/homePage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/logo.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
