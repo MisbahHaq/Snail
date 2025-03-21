717e12b53827c6ada731839611518af54241b919
Misbah Haque <misbahmuneer2003@gmail.com>
2025-03-21 11:18:20 +0500

    Update

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -13,45 +13,49 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       extendBodyBehindAppBar: false,
       appBar: AppBar(backgroundColor: Colors.transparent, elevation: 3),
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset("assets/images/bg.jpg", fit: BoxFit.cover),
-          ),
-          Column(
-            children: [
-              SizedBox(height: 70),
-              Center(
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: 110,
-                  height: 110,
-                  fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset("assets/images/bg.jpg", fit: BoxFit.cover),
+            ),
+            Column(
+              children: [
+                SizedBox(height: 70),
+                Center(
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 110,
+                    height: 110,
+                    fit: BoxFit.contain,
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "S ' N A I L",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          fontSize: 20,
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "S ' N A I L",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Text("Your Personal Inventory")],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text("Your Personal Inventory")],
+                    ),
+                  ],
+                ),
+                Image.asset('assets/images/mid.png'),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
